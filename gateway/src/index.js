e021476d6c2bdc8293771c985a86b0c87c9f2217
Lemar Abad <xdlemar15@gmail.com>
2025-09-09 00:54:15 +0800

    testingann bi

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -16,7 +16,8 @@ function authRequired(req, res, next) {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.user = payload;
     next();
-  } catch {
+  } 
+  catch {
     return res.status(401).json({ error: "Invalid token" });
   }
 }
